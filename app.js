d5b1fc870a50cb24695e5b48e85b620a24be0d7e
caroline burns <carolineburns@carolines-MacBook.local>
2019-01-17 10:50:54 -0700

    did sprint 1 and 2

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3003
+const data = require('./data.json')
+
+app.use(express.static('public'))
+
+app.get('/data', (req, res, next) => {
+    res.status(200).send({
+        "message": 'success!',
+        "data": data
+    })
+})
+
+
+app.get('/:tag', (req, res, next) => {
+    res.status(200).send(req.params.tag)
+})
+
+app.listen(port, () => console.log(`Porty on ${port}`))
